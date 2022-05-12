c2cd60371fe3b919ba00ebdfddb354a3f71ddfce
Sarah.Mn <sarah.mohammadian95@gmail.com>
2022-05-12 16:33:07 +0430

    base app

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+
+<div v-if="state.pokemon" class="select-none max-w-sm m-auto rounded overflow-hidden shadow-lg bg-white flex justify-center flex-col items-center">
+  <h3 class="text-2xl font-bold text-green-900 uppercase my-5">{{state.pokemon.name}}</h3>
+    <!-- <img class="w-full" :src="img" alt="Sunset in the mountains"> -->
+    <div class="flex justify-center w-full">
+      <img :src="state.pokemon.sprites.front_shiny" alt="" class="w-48 drop-shadow-3xl">
+      <img :src="state.pokemon.sprites.back_shiny" alt="" class="w-48 drop-shadow-3xl">
+    </div>
+
+    <div class="px-6 py-10">
+      <div class="font-bold text-xl mb-2">Types</div>
+      <p class="text-gray-700 text-xl flex flex-col items-center justify-center" v-for="(type, inx) in state.pokemon.types" :key="inx">
+          {{type.type.name}}
+      </p>
+    </div>
   </div>
+
 </template>
+<script setup>
+import { useRoute } from 'vue-router';
+import { reactive, onMounted, toRefs } from 'vue';
+import axios from 'axios';
+
+const state = reactive({
+  pokemon: null,
+  loading: false,
+  data: {
+    text: "iejiejfiejfieeij"
+  }
+  
+  })
+
+const route = useRoute();
+
+onMounted(()=>{
+  try {
+    state.loading = true;
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${route.params.slug}/`).then(
+ res => {
+   console.log(res.data);
+   state.pokemon = res.data
+ }
+)
+  } catch (error) {
+    console.error(error);
+  } finally {
+    state.loading = false;
+  }
+
+return { ... toRefs(state)}
+
+})
+
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,53 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="w-full flex justify-center">
+  <input type="text" v-model="state.text" placeholder="Enter pokemon here" class="mt-10 pr-10 pl-2 outline-none py-2 border-blue-500 rounded">
+</div>
+<div class="mt-10 p-4 flex flex-wrap justify-center">
+  <div v-for="(pokemon, inx) in state.filteredPokemon" :key="inx" class="ml-4 text-2x text-blue-500">
+  <router-link :to="`/about/${state.urlIdLookup[pokemon.name]}`">
+  
+  {{pokemon.name}}
+  </router-link>
   </div>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import { reactive, toRefs, computed } from '@vue/reactivity';
+import axios from 'axios';
+import { onMounted } from 'vue';
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+const state = reactive({
+  pokemons: [],
+  urlIdLookup: {},
+  text: "",
+  filteredPokemon: computed(() => updatePokemon())
+})
+
+const updatePokemon = () => {
+  if(!state.text) {
+    return [];
   }
+
+  return state.pokemons.filter((pokemon) => 
+    pokemon.name.includes(state.text)
+  );
 }
+
+onMounted(()=>{
+  axios.get("https://pokeapi.co/api/v2/pokemon?offset=0").then(
+ res => {
+   console.log(res.data);
+   state.pokemons = res.data.results;
+   state.urlIdLookup = res.data.results.reduce((acc, curr, inx) => 
+   acc = {...acc, [curr.name] : inx + 1}
+   ,{});
+ }
+)
+
+return { ... toRefs(state)}
+
+})
+
+
 </script>
